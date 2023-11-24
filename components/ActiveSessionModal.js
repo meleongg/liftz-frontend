4394534ce60406c4428f7c44925ed78bae7f376b
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2023-11-23 19:41:29 -0800

    Add ActiveSessionModal

---
+++ components/ActiveSessionModal.js
@@ -0,0 +1,44 @@
+import {
+  Link,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+
+export const ActiveSessionModal = ({
+  isOpen,
+  onClose,
+  userId,
+  workoutName,
+  workoutId,
+}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size="sm">
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>You have an active {workoutName} session!</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          Please cancel or end this{" "}
+          <Link
+            fontWeight={600}
+            textDecoration={"underline"}
+            color="blue.50"
+            as={NextLink}
+            href={`/authenticated/${userId}/workouts/${workoutId}/session`}
+          >
+            session
+          </Link>{" "}
+          before starting a new one.
+        </ModalBody>
+
+        <ModalFooter></ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};

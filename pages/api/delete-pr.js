80641596ceda3d1bed02168b21bfd6a8efecd19e
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2023-12-26 23:49:24 -0800

    Group PRs by workout

--- pages/api/delete-pr.js
+++ pages/api/delete-pr.js
@@ -1,26 +1,21 @@
 const BE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export default async function (req, res) {
-    const { prId } = req.body;
+  const { prId } = req.body;
 
-    const data = {
-        prId: prId,
-    };
+  try {
+    const rawResponse = await fetch(`${BE_URL}/stats/${prId}/delete`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    const deleteMsg = await rawResponse.json();
 
-    try {
-        const rawResponse = await fetch(`${BE_URL}/prs/${prId}/delete`, {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        });
-        const deleteMsg = await rawResponse.json();
-
-        res.status(200).json(deleteMsg);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: "Error deleting PR" });
-    }
+    res.status(200).json(deleteMsg);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error deleting PR" });
+  }
 }
--- pages/authenticated/[user]/stats.js
+++ pages/authenticated/[user]/stats.js
@@ -1,184 +1,225 @@
-import { Box, Spinner, Text, Button } from "@chakra-ui/react";
-import React, { useState, useEffect } from "react";
+import { Box, Button, Spinner, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import PRChart from "../../../components/PRChart";
 
+import Head from "next/head";
 import Navbar from "../../../components/Navbar";
 import Title from "../../../components/Title";
-import PRChart from "../../../components/PRChart";
-import Head from "next/head";
-import { v4 as uuidv4 } from "uuid";
 
 const metadata = {
-    title: "PR Tracker | liftz",
-    description: "PR Tracker page",
-    image: "https://liftz-workout-tracker.vercel.app/meta-image.png",
+  title: "PR Tracker | liftz",
+  description: "PR Tracker page",
+  image: "https://liftz-workout-tracker.vercel.app/meta-image.png",
 };
 
 const Stats = ({ dbPrs, error }) => {
-    const [loading, setLoading] = useState(true);
-    const [prs, setPrs] = useState(dbPrs);
+  const [loading, setLoading] = useState(true);
+  const [prs, setPrs] = useState([]);
 
-    const router = useRouter();
-    const userId = router.query.user;
+  const router = useRouter();
+  const userId = router.query.user;
 
-    useEffect(() => {
-        setLoading(false);
+  useEffect(() => {
+    const groupedPrs = dbPrs.reduce((acc, pr) => {
+      const { name, _id } = pr.workout;
+      const existingWorkout = acc.find((item) => item.name === name);
+
+      if (!existingWorkout) {
+        acc.push({ name, _id: _id, prs: [pr] });
+      } else {
+        existingWorkout.prs.push(pr);
+        existingWorkout.prs.sort((a, b) =>
+          a.exercise.localeCompare(b.exercise)
+        );
+      }
+
+      return acc;
     }, []);
 
-    if (loading) {
-        return (
-            <Box
-                textAlign="center"
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                w="100%"
-                h="100vh"
-                color="blue.50"
-            >
-                <Spinner />
-            </Box>
-        );
-    }
+    setPrs(groupedPrs);
+    setLoading(false);
+  }, [dbPrs]);
 
-    if (error) {
-        return (
-            <Box
-                textAlign="center"
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                w="100%"
-                h="100vh"
-                color="#333"
-            >
-                <Text>Failed to load data</Text>
-            </Box>
-        );
-    }
+  if (loading) {
+    return (
+      <Box
+        textAlign="center"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        w="100%"
+        h="100vh"
+        color="blue.50"
+      >
+        <Spinner />
+      </Box>
+    );
+  }
 
-    const handleDeletePRClick = async (index) => {
-        const updatedPrs = [...prs];
-        updatedPrs.splice(index, 1);
-        setPrs(updatedPrs);
+  if (error) {
+    return (
+      <Box
+        textAlign="center"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        w="100%"
+        h="100vh"
+        color="#333"
+      >
+        <Text>Failed to load data</Text>
+      </Box>
+    );
+  }
 
-        const data = {
-            prId: prs[index]._id,
-        };
+  const handleDeletePRClick = async (workoutName, prId) => {
+    const updatedPrs = [...prs];
 
-        try {
-            const rawResponse = await fetch("/api/delete-pr", {
-                method: "POST",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            });
-            await rawResponse.json();
-        } catch (err) {
-            console.log(err);
+    // Find the index of the workout object in the prs array based on the workout name
+    const workoutIndex = updatedPrs.findIndex(
+      (workout) => workout.name === workoutName
+    );
+
+    if (workoutIndex !== -1) {
+      // Find the PR index in the prs array of the specific workout based on the prId
+      const prIndex = updatedPrs[workoutIndex].prs.findIndex(
+        (pr) => pr._id === prId
+      );
+
+      if (prIndex !== -1) {
+        // Remove the PR from the workout's prs array
+        updatedPrs[workoutIndex].prs.splice(prIndex, 1);
+
+        // If the prs array for the workout becomes empty, remove the entire object
+        if (updatedPrs[workoutIndex].prs.length === 0) {
+          updatedPrs.splice(workoutIndex, 1);
         }
+      }
+    }
+
+    // Update the state with the modified prs array
+    setPrs(updatedPrs);
+
+    const data = {
+      prId: prId,
     };
 
-    return (
-        <Box minHeight="100vh" mb={["80px", "80px", "0px", "0px", "0px"]}>
-            <Head>
-                <title>{metadata.title}</title>
-                <meta name="description" content={metadata.description} />
-                <meta name="image" content={metadata.image} />
+    try {
+      const rawResponse = await fetch("/api/delete-pr", {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      await rawResponse.json();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-                <meta
-                    property="og:url"
-                    content="https://liftz-workout-tracker.vercel.app/"
-                />
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content={metadata.title} />
-                <meta
-                    property="og:description"
-                    content={metadata.description}
-                />
-                <meta property="og:image" content={metadata.image} />
+  return (
+    <Box minHeight="100vh" mb={["80px", "80px", "0px", "0px", "0px"]}>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="image" content={metadata.image} />
 
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:title" content={metadata.title} />
-                <meta
-                    name="twitter:description"
-                    content={metadata.description}
-                />
-                <meta name="twitter:image" content={metadata.image} />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            {/* 80px is the navbar */}
-            <Box
-                minHeight="calc(100vh - 80px)"
-                h="calc(100% - 80px)"
-                pt="30px"
-                pl={["10px", "50px", "100px", "100px", "100px"]}
-                pr={["10px", "50px", "100px", "100px", "100px"]}
-                pb="80px"
-            >
-                <Title userId={userId} content={"PR Progress"} />
-                <Box mt="20px" display="flex" flexDirection="column" mb="20px">
-                    <Box
-                        borderRadius="20px"
-                        p="14px"
-                        bgColor="blue.50"
-                        color="white"
-                        mt="20px"
-                        mb="20px"
-                    >
-                        <Text fontSize="20px" fontWeight="700">
-                            PRs are automatically created for each of your
-                            exercises. Each PR will update every time you
-                            complete a session where you hit a new PR!
-                        </Text>
-                    </Box>
-                    {prs?.map((pr, index) => {
-                        return (
-                            <Box key={uuidv4()} mt="20px" mb="20px">
-                                <PRChart pr={pr} />
-                                <Button
-                                    backgroundColor="blue.50"
-                                    color="white"
-                                    _hover={{
-                                        cursor: "pointer",
-                                        bg: "lightBlue.50",
-                                    }}
-                                    onClick={() => handleDeletePRClick(index)}
-                                >{`Delete ${pr.exercise} PR`}</Button>
-                            </Box>
-                        );
-                    })}
+        <meta
+          property="og:url"
+          content="https://liftz-workout-tracker.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.image} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* 80px is the navbar */}
+      <Box
+        minHeight="calc(100vh - 80px)"
+        h="calc(100% - 80px)"
+        pt="30px"
+        pl={["10px", "50px", "100px", "100px", "100px"]}
+        pr={["10px", "50px", "100px", "100px", "100px"]}
+        pb="80px"
+      >
+        <Title userId={userId} content={"PR Progress"} />
+        <Box mt="20px" display="flex" flexDirection="column" mb="20px">
+          <Box
+            borderRadius="20px"
+            p="20px"
+            bgColor="blue.50"
+            color="white"
+            mt="20px"
+            mb="20px"
+          >
+            <Text fontSize="18px" fontWeight="700">
+              PRs are created for each of your completed workout session
+              exercises.
+            </Text>
+            <Text fontSize="18px" fontWeight="700" mt="16px">
+              Each PR will automatically update every time you complete a
+              workout session where you hit a new PR!
+            </Text>
+          </Box>
+          {prs.map((workout) => (
+            <Box key={workout._id} mt="20px" mb="20px">
+              <Text fontWeight="700" textAlign="center">
+                {workout.name}
+              </Text>
+              {workout.prs.map((pr) => (
+                <Box key={pr._id} mt="20px" mb="20px">
+                  <PRChart pr={pr} />
+                  <Button
+                    backgroundColor="blue.50"
+                    color="white"
+                    _hover={{
+                      cursor: "pointer",
+                      bg: "lightBlue.50",
+                    }}
+                    onClick={() => handleDeletePRClick(workout.name, pr._id)}
+                  >{`Delete ${pr.exercise} PR`}</Button>
                 </Box>
+              ))}
             </Box>
-            <Navbar userId={userId} currPage="stats" />
+          ))}
         </Box>
-    );
+      </Box>
+      <Navbar userId={userId} currPage="stats" />
+    </Box>
+  );
 };
 
 export async function getServerSideProps(context) {
-    const BE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
-    const { user } = context.params;
+  const BE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+  const { user } = context.params;
 
-    try {
-        const response = await fetch(`${BE_URL}/stats/${user}`);
-        const data = await response.json();
+  try {
+    const response = await fetch(`${BE_URL}/stats/${user}`);
+    const data = await response.json();
 
-        return {
-            props: {
-                dbPrs: data,
-            },
-        };
-    } catch (err) {
-        console.log(err);
+    return {
+      props: {
+        dbPrs: data,
+      },
+    };
+  } catch (err) {
+    console.log(err);
 
-        return {
-            props: {
-                error: true,
-            },
-        };
-    }
+    return {
+      props: {
+        error: true,
+      },
+    };
+  }
 }
 
 export default Stats;
